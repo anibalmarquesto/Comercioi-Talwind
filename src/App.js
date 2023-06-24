68b87013d80b6bf4b97c8a47fcdc25ebe4f6fa7a
mario marquesto <mariomarquestso@gmail.com>
2023-06-24 00:46:29 -0300

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Nabvar from "./components/Nabvar";
+import Product1 from "./components/Product1";
+
+function App() {
+  return (
+    <>
+      <Nabvar />
+      <Product1/>
+    </>
+  );
+}
+
+export default App;
